8cafeff8e7cecad1c5afe8a1f50b67f5ef1c857d
Nicolas <nic_rf@hotmail.com>
2019-02-23 20:14:21 -0500

    Normal working - Not merge
    Some mesh dont load, but it is fine for now

--- VRMLModel.js
+++ VRMLModel.js
@@ -8,12 +8,11 @@
 {
 
     xeogl.VRMLModel = class xeoglVRMLModel extends xeogl.Model {
-
-
         init(cfg) {
             super.init(cfg);
             this._src = null;
             this.src = cfg.src;
+            this.debug = false;
         }
 
 
@@ -125,7 +124,8 @@
 
 //--------------------------------------------------------------------------------------------
 // Loads VRML
-//
+// Build for EPLAN P8 Export
+// By Nicolas Fournier (nic_rf)
 // Use https://github.com/bartmcleod/VrmlParser libs
 //
 // Originally based on the THREE.js VRML loaders: 
@@ -137,7 +137,6 @@
 //--------------------------------------------------------------------------------------------
 
     var load = function (model, url, ok) {
-
         loadFile(url, function (text) {
                 var state = parse(text, model, url);
                 ok(state);
@@ -147,8 +146,13 @@
             });
     };
 
-	function parse(data, model, options) {		
-
+	function parse(data, model, options) {
+		var debug = options.debug ? options.debug : false;
+		var showInfo = options.showInfo ? options.showInfo : true;
+		var defines = {};
+		var totalNode = 0;
+		var doneNode = 0;
+		
         const WebGLConstants = {
             34963: 'ELEMENT_ARRAY_BUFFER',  //0x8893
             34962: 'ARRAY_BUFFER',          //0x8892
@@ -180,43 +184,40 @@
             'MAT3': 9,
             'MAT4': 16
         };
-		var defines = {};
 		
-		function addShape(data,parent,model) {			
-			var mat = loadMaterial(data,parent,model);
-			var geometry = getGeometry(data.geometry);//loadGeometry(data,parent,model);
-			if (geometry && mat) {
-				return new xeogl.Mesh({
+		function addShape(data,parent) {	//model?		
+			var mat = loadMaterial(data,parent);
+			var geometry = buildGeometry(data.geometry);//loadGeometry(data,parent,model);
+			//model._addComponent(mat)
+			var mesh = new xeogl.Mesh({
 					geometry:geometry,
-					material:mat
-				});	
-			} else if (geometry) {
-				return new xeogl.Mesh({
-					geometry:geometry
-				});
-			}
+					material: mat
+			});
+			return mesh;
 		}
 		
-		function loadGeometry(data,parent,model) {		
+		function loadGeometry(data,parent) {		
 			var geometrysInfo = data.geometry;
 			if (geometrysInfo) {
 				if (Array.isArray(geometrysInfo)){
 					// group?
-					var group = new xeogl.Group();
+					/*var group = new xeogl.Group();
 					for (var i = 0, len = materialsInfo.length; i < len; i++) {
-						var geometry = getGeometry(geometrysInfo[i]);
+						var geometry = buildGeometry(geometrysInfo[i]);
 						group.addChild(geometry);
 						model._addComponent(geometry);	
 					}
-					return group;							
+					return group;	*/						
 				} else {
-					return getGeometry(geometrysInfo);
+					return buildGeometry(geometrysInfo);
 				}
 			}			
 		}
 		
-		function getGeometry(data) {
-			console.log("Shape as : "+ data.node);
+		function buildGeometry(data) {
+			if (this.debug === true ) {
+				console.log("Shape as : "+ data.node);
+			}
 			if ( data.node === 'Box' ) {
 				var s = data.size;
 				return new xeogl.BoxGeometry({ // Half-size on each axis; BoxGeometry is actually two units big on each side.
@@ -254,7 +255,7 @@
 						indices: indices
 					});
 				}
-			} else if (data.node === 'IndexedLineSet') {	//To be test
+			} else if (data.node === 'IndexedLineSet') {	//To be check , each array need to be a line system
 				var positions = [];
 				var indices = []; 
 				if (data.coord) {
@@ -269,61 +270,74 @@
 					});
 				}
 			} else if (data.node === 'IndexedFaceSet') {	//To be done
-				var positions = [];
+			var positions = [];
 				var indices = [];
+				var uvs = [];				
 				var faces = [];
+				var face_uvs=[[0,0],[1,0],[1,1],[0,1]];
 				if (data.coord) {
-
+					// positions
+					if ( data.texCoord) {
+						uvs = data.texCoord.point;
+					}
 					for (var i = 0; i < data.coord.point.length; i ++ ) {
+						if (!data.texCoord) {
+							uvs.push(data.coord.point[i]);
+						}
 						positions.push(data.coord.point[i].x,data.coord.point[i].y,data.coord.point[i].z);
 					}
-					/*for (var i = 0; i < data.coordIndex.length; i ++ ) {
-						indices.push(data.coordIndex[i].join(", "));
-					}*/
-					if (data.ccw && data.ccw === false)
-						console.error("CCW")
-					var skip = 0;
-					for ( var i = 0, j = data.coordIndex.length; i < j; i ++ ) {
-						var indexes = data.coordIndex[i];
-						skip = 0;
-						while (indexes.length >= 3 && skip < (indexes.length - 2)) {
-							var a = indexes[0];
-							var b = indexes[skip + (data.ccw ? 1 : 2)];
-							var c = indexes[skip + (data.ccw ? 2 : 1)];
-							skip++;
-							faces.push(a, b, c);
-						}
-					}
-					indices = faces.toString().split(",");
-					var creaseAngle = data.creaseAngle ? data.creaseAngle : 2;
-					return new xeogl.Geometry({
-						primitive: "triangles",
-						positions: new Float32Array(positions),
-						indices: new Uint16Array(indices),
-						autoVertexNormals :true,
-						edgeThreshold:2
-					});
 				}
-				//return parseIndexedFaceSet(data);
+				if (data.coordIndex && data.coordIndex.length && data.coordIndex.length>0) {
+					//Bug when we got -1 coordIndex to separate indices for each polygon - To be done - But EPLAN do not created face with multiple polygone
+					// indices from faces		  
+					for (var f = 0; f < data.coordIndex.length; f++) {
+					  for(var j = 0; j < data.coordIndex[f].length; j++) {
+						uvs=uvs.concat(face_uvs[j]);
+					  }
+					  for (i = 0; i < data.coordIndex[f].length - 2; i++) {
+						  indices.push(data.coordIndex[f][0], data.coordIndex[f][i + 2], data.coordIndex[f][i + 1]);
+					  }
+					}
+				}
+				var normals = [];
+				positions = new Float32Array(positions);
+				indices = new Uint16Array(indices)				
+				//Build Normals
+				normals = xeogl.math.buildNormals(positions,indices,normals);				
+				var result =  xeogl.math.mergeVertices(positions, normals, null, indices)
+				result.normals = xeogl.math.buildNormals(result.positions,result.indices,result.normals);
+				var creaseAngle = data.creaseAngle ? data.creaseAngle : 2;
+				/*if (result.positions && result.positions.length && result.positions.length > 0) {					
+					result = xeogl.math.faceToVertexNormals(result.positions, result.normals, {smoothNormalsAngleThreshold : creaseAngle}); //Not working?
+				}*/			
+				return new xeogl.Geometry({
+						primitive: "triangles",
+						positions: result.positions,
+						indices: result.indices,
+						normals: result.normals,
+						//autoVertexNormals :true,
+					});
 			}
 		}		
 
 
-		function loadMaterial(data,parent,model) {
+		function loadMaterial(data,parent) {
 			var appearance = data.appearance; //child??
 			if (appearance) {
 				var materialsInfo = appearance.material;
 				var material;
 				if (materialsInfo) {
-					if (Array.isArray(materialsInfo)){
+					/*if (Array.isArray(materialsInfo)){
 						for (var i = 0, len = materialsInfo.length; i < len; i++) {
 							material = loadMaterialColorize(materialInfo[i]); //As option? is not use specularColor						
 							//parent.addChild(material);
 							model._addComponent(material);
 						}
-					} else {
-						return loadMaterialColorize(materialsInfo); //As option? is not use specularColor							
-					}
+					} else {*/
+						material =  loadMaterialColorize(materialsInfo); //As option? is not use specularColor							
+						//model._addComponent(material);
+						return material;
+					//}
 				}
 			}			
 			//ImageTexture tbd
@@ -332,7 +346,7 @@
 		function loadMaterialColorize(materialInfo) {
 			var mat = new xeogl.LambertMaterial();					
             if (materialInfo.diffuseColor){
-			//	mat.ambient =   [materialInfo.diffuseColor.x,materialInfo.diffuseColor.y, materialInfo.diffuseColor.z]
+				mat.ambient =   [materialInfo.diffuseColor.x,materialInfo.diffuseColor.y, materialInfo.diffuseColor.z]
 				mat.color =   [materialInfo.diffuseColor.x,materialInfo.diffuseColor.y, materialInfo.diffuseColor.z]
 			}
 			
@@ -343,52 +357,77 @@
 			if (materialInfo.transparency) {
 				mat.alpha = materialInfo.transparency;
 			}
-			
-			if ( materialInfo.string ) {
-				defines[ materialInfo.string ] = mat;
-			}	
 			return mat;
         }
 
-		function parseNode(data,parent,model) {
-			console.log("Parse an node " + data.node);
+		function parseNode(data,parent) {
+			var name = "";	
 			if (data.name) {
-				console.log("Parse an node " + data.name);
+				name = data.name;	
+			} else if (data.node){
+				name = data.node;
+			}
+			if (this.debug === true ) {
+				console.log("Parse an node " + data.node);
+				if (name) {
+					console.log("Parse an node " + data.name);
+				}
 			}
 			var object = parent;
 			switch(data.node) {
 				case 'Transform' :
 				case 'Group' :
-					object = new xeogl.Group();
+					object = new xeogl.Group();									
+					if (data.rotation) {
+						var r = data.rotation;
+						object.matrix= xeogl.math.rotationMat4v(r.radians,[ r.x , r.y, r.z ]);
+					}					
 					if (data.translation) {
 						var t = data.translation;
 						object.position = [ t.x, t.y, t.z ];
-					}
-					if (data.rotation) {
-						var r = data.rotation;
-						object.quaternion= [ r.x, r.y, r.z , r.w ];
 					}
 					if (data.scale) {
 						var s = data.scale;
 						object.scale = [ s.x, s.y, s.z ];
 					}
-					if (data.name) {
-						object.id = data.name;
-						defines[ object.id ] = object;
-					}
-					parent.addChild(object); // Don't automatically inherit properties
-					model._addComponent(object);
 					break;
 				case 'Shape':
-					shape = addShape(data,parent,model);
-					if (shape) {
-						if (data.name) {
-							shape.id = data.name;
-							defines[ shape.id ] = shape;
+					object = addShape(data,parent);
+					break;
+				case 'DirectionalLight':	//ambientIntensity 
+					if (data.on) {
+						object = new xeogl.DirLight({
+						   dir: [data.direction.x, data.direction.y, data.direction.z],
+						});						
+						if (data.color) {
+							object.color = [data.color.x, data.color.y, data.color.z];
+						}	
+						if (data.intensity) {
+							object.intensity = data.intensity;
 						}
-						parent.addChild(shape);
-						model._addComponent(shape);
 					}
+					break;
+				case 'PointLight':
+					if (data.on) {
+						object = new xeogl.PointLight({
+						   pos: [data.location.x, data.location.y, data.location.z],
+						});						
+						if (data.color) {
+							object.color = [data.color.x, data.color.y, data.color.z];
+						}	
+						if (data.intensity) {
+							object.intensity = data.intensity;
+						}					
+					}
+					break;
+				case 'IndexedFaceSet':
+				case 'IndexedLineSet':
+				case 'PointSet':
+				case 'Sphere':
+				case 'Cone':
+				case 'Cylinder':
+				case 'Box':
+					object = buildGeometry(data.geometry,parent);
 					break;
 				case 'Light':
 				case 'AmbientLight':
@@ -410,79 +449,52 @@
 					console.error("Node is not defined")
 					break;
 			}
+			if (parent != object) {
+				object.id = name;
+				defines[ object.id ] = object;
+				model._addComponent(object);
+				if (parent !== undefined) {
+					parent.addChild(object);
+				}
+			}
 			if (data.children) {
 				for ( var i = 0, l = data.children.length; i < l; i ++ ) {
-					parseNode( data.children[ i ], object,model );
+					parseNode( data.children[ i ], object );
 				}
 			}
+			doneNode++;
+			if (this.showInfo) {
+				console.info("Node complete %d", this.doneNode/this.totalNode);
+			}
 		}
-
-		function parseNodeDefinition(data,model) {
-			if (data.node!= 'Transform' && data.node!= 'Group')
-				return;
-
-			var object = new xeogl.Group();
-			if (data.translation) {
-				var t = data.translation;
-				object.position = [t.x,t.y,t.z]
-			}
-
-			if (data.rotation) {
-				var r = data.rotation;
-				object.quaternion= [ r.x, r.y, r.z , r.w ];
-			}
-			if (data.scale) {
-				var s = data.scale;
-				object.scale = [ s.x, s.y, s.z ];
-			}
-			if (data.name) {
-				object.id = data.name;
-				defines[ object.id ] = object;
-			}
-
-			for (var i = 0, l = data.children.length; i < l; i ++ ) {
-				switch(data.children[ i ].node) {
-					case 'Transform' :
-					case 'Group' :
-						//TBD
-						break;
-					case 'Shape':
-						var shape = addShape(data.children[ i ]);
-						if (shape) {
-							if (data.children[ i ].name) {
-								shape.id = data.children[ i ].name;
-								defines[ shape.id ] = shape;
-							}
-							object.addChild(shape, false);
-							//model._addComponent(shape);
-						}
-						break;
+		
+		function countNode (obj) {
+			var count = 0;
+			for (var property in obj) {
+				if (Object.prototype.hasOwnProperty.call(obj, property)) {
+					count++;
 				}
 			}
-			model._addComponent(object);
+			return count;
 		}
+
 
 		// Action
 		model.clear();
-		var tree = vrmlParser.parse(data);
-		//var lines = parseLines(data);
-		console.log(tree);
-		for ( var i = 0, l = tree.length; i < l; i ++ ) {
-			parseNode(tree[i],model,model);
+		if (this.debug === true ) {
+			console.log("Parse the file");
 		}
-		console.log(defines);
-		/*for (node in tree.nodeDefinitions){
-			parseNodeDefinition(node,model);
-		}*/
-		//for
-		/*jQuery.each(tree.nodeDefinitions, function() {
-			console.log(this.name);
-			parseNodeDefinition(this,model);
-		});*/
-		//forEach
-
-       // return ;
-
+		var tree = vrmlParser.parse(data);
+		if (this.debug === true ) {
+			console.log(tree);
+		}
+		this.totalNode = countNode(tree.nodeDefinitions);
+		for ( var i = 0, l = tree.length; i < l; i ++ ) {
+			parseNode(tree[i],model);
+		}
+		if (this.debug === true ) {
+			console.log(defines);
+		}
     }
 
 
